4b027310654893de284e93bd7c61d06fc629be5c
Bryce Sellars <sellabs1@student.op.ac.nz>
2014-09-19 19:41:42 +1200

    iframe url

--- game.php
+++ game.php
@@ -38,7 +38,9 @@
 
 	<div id ="container">
 
-		<?php echo "<iframe src='http://".$_POST['serverAddress'].":".$_POST['serverPort']."?username=".$_SESSION['username']."></iframe>"; ?>
+		<?php echo "<iframe src='http://".$_POST['serverAddress'].":".$_POST['serverPort']."?username=".$_SESSION['username']."></iframe>";
+		echo "titty sprinkles";
+		 ?>
 
 	</div>
 
