98409c5eb261873bae32c631c998eb27e853b20e
Bryce Sellars <sellabs1@student.op.ac.nz>
2014-09-21 23:38:36 +1200

    Layout Design

--- game.php
+++ game.php
@@ -27,18 +27,7 @@
 
 <body>
 
-	<div id ="container">
-	
-		<div id ="nav">
-			<form method="POST" action="userHome.php">
-				<ul>
-					<li id="menu-tutorial"><a href="tutorial.php"></a></li>
-					<li id="menu-help"><a href="help.php"></a></li>
-					<li id="menu-settings"><a href="settings.php"></a></li>
-					<li id="menu-exit"><input type="submit" class="logout-button" value="" name="logout"></li>
-				</ul>
-			</form>
-		</div>
+	<div id ="game-container">
 
 		<?php echo "<iframe src='http://".$_POST['serverAddress'].":".$_POST['serverPort']."?username=".$_SESSION['username']."'></iframe>" ?>
 
