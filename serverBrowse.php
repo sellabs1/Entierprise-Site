f470ca0c5102a8091371b732ed0fde8f5bd08657
Bryce Sellars <sellabs1@student.op.ac.nz>
2014-09-22 00:59:38 +1200

    Server Browser

--- serverBrowse.php
+++ serverBrowse.php
@@ -44,10 +44,10 @@
 
 			<div id="server-back">
 				<form id="back-form" method="POST" action="#">
-					<input type="submit" value="Back" name="Back">
+					<input type="submit" value=" " name="Back">
 				</form>
 			</div>
-			
+
 		</div>
 
 	</div>
