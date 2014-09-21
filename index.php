cbb3e2fc5f4875b19c70839214f83326af47f8a8
Bryce Sellars <sellabs1@student.op.ac.nz>
2014-09-21 21:51:04 +1200

    Layout Design

--- index.php
+++ index.php
@@ -25,10 +25,10 @@
 			<form method="post" action="index.php">
 				<input type="text" name="username" placeholder="Username"/><br>
 				<input type="password" name="password" placeholder="Password"/><br>
-				<input type="submit" value="Submit" id="login-button" name="submit"/>
+				<input type="submit" value=" " id="login-button" name="submit"/>
 			</form>
 			<br>
-			<a href="register.php">Register Here</a>
+			<a id="register-link" href="register.php">Register Here</a>
 		</div>
 	</div>
 </body>
