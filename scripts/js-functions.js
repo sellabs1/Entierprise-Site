bb67646e58e9ee1eb52a01e81cd1061aa270ea65
Bryce Sellars <sellabs1@student.op.ac.nz>
2014-09-21 21:37:52 +1200

    Layout Design

--- scripts/js-functions.js
+++ scripts/js-functions.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 	$("#regUsername").keyup(function (e) { //user types username on inputfiled
 
    		var username = $(this).val(); //get the string typed by user
-   		var button = $('#button');
+   		var button = $('#reg-button');
 
    		$.post('usernameCheck.php', {'username':username}, function(data) { //make ajax call to usernameCheck.php
   			if(data == "1"){
@@ -44,7 +44,7 @@ $(document).ready(function(){
 
 		var pass = $("#pass").val();
 		var confPass = $("#confPass").val();
-		var button = $('#button');
+		var button = $('#reg-button');
 
 		if(pass != confPass){
 			$("#pass-result").html("<img src='../images/passMatch.gif' />").fadeIn();
