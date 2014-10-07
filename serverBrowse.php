784b3bdf268e2256cb45f9a52441bf5f1f31700c
Bryce Sellars <sellabs1@student.op.ac.nz>
2014-10-07 14:31:32 +1300

    Title styling

--- serverBrowse.php
+++ serverBrowse.php
@@ -35,7 +35,9 @@
 
 	<div id="container">
 
-		<div id="name-cloud"></div>
+		<div id="name-cloud">
+			<div id="title"><h1 id='welcome'><?php echo "Welcome, ".$_SESSION['username']; ?></h1></div>
+		</div>
 
 		<div id="serverList">
 
