cb3754e2bc09bff37c0df5ad2626705bc251625b
Bryce Sellars <sellabs1@student.op.ac.nz>
2014-10-11 16:02:08 +1300

    Tinkering with register jquery function

--- scripts/js-functions.js
+++ scripts/js-functions.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 	var userFlag = 0;
 	var intputElements = document.getElementsByTagName("INPUT");
 
-	$("#regUsername").keyup(function (e) { //user types username on inputfiled
+	$("#regUsername").keyup(function (e) { 
 
    		var username = $(this).val(); //get the string typed by user
    		var button = $('#reg-button');
@@ -35,6 +35,29 @@ $(document).ready(function(){
 
 	//Tests to see whether the user has input the same passwords for registering
 	$("#confPass").on('keyup', function(){
+
+		var pass = $("#pass").val();
+		var confPass = $("#confPass").val();
+		var button = $('#reg-button');
+
+		if(pass != confPass){
+			$("#pass-result").html("<img src='../images/passMatch.png' />").fadeIn();
+			passFlag = 0;
+		}
+		else{
+			$("#pass-result").html("");
+			passFlag = 1;
+		};
+
+		if(userFlag == 1 && passFlag == 1){
+   			button.prop('disabled', false);
+   		}
+   		else{
+   			button.prop('disabled', true);
+   		}
+	});
+
+	$("#pass").on('keyup', function(){
 
 		var pass = $("#pass").val();
 		var confPass = $("#confPass").val();
